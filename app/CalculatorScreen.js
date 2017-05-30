4ed748a1564b7ccfd507e2d300d16b68a8d8beaa
Yura Savchuk <us@seotm.com>
2017-05-30 14:15:02 +0300

    Add buttons to layout.

--- app/CalculatorScreen.js
+++ app/CalculatorScreen.js
@@ -4,20 +4,118 @@ import {
   TextInput,
   View,
   Text,
-  Platform
+  Platform,
+  TouchableHighlight,
+  ScrollView
 } from 'react-native';
 import StatusBar from './components/StatusBar'
 
 export default class CalculatorScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.conduction = "33 - 4"
+  }
+
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <StatusBar backgroundColor="#5E8D48" barStyle="light-content" />
         <Text style={styles.appBar}>Калькулятор</Text>
         <View style={styles.content} >
-          <Text style={styles.textInput}>0</Text>
+          <Text style={styles.textInput}>{this.conduction}</Text>
+          <View style={styles.buttonsRow}>
+            <TouchableHighlight style={styles.buttonLeft}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>C</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>&larr;</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>%</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.buttonRight}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>&divide;</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.buttonsRow}>
+            <TouchableHighlight style={styles.buttonLeft}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>7</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>8</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>9</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.buttonRight}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>&times;</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.buttonsRow}>
+            <TouchableHighlight style={styles.buttonLeft}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>4</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>5</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>6</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.buttonRight}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>-</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.buttonsRow}>
+            <TouchableHighlight style={styles.buttonLeft}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>1</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>2</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>3</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.buttonRight}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>+</Text>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.buttonsRow}>
+            <TouchableHighlight style={styles.buttonLeft}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>00</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>0</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>.</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.buttonEquals}
+                underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>=</Text>
+            </TouchableHighlight>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -52,5 +150,59 @@ const styles = StyleSheet.create({
   },
   headerTitle: {
     backgroundColor: 'blue'
+  },
+  buttonText: {
+    fontSize: 30,
+    color: 'black',
+    alignSelf: 'center'
+  },
+  buttonLeft: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#F0F0F0',
+    borderColor: '#000000',
+    borderWidth: 1,
+    borderRadius: 8,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  button: {
+    flex: 1,
+    marginLeft: 10,
+    flexDirection: 'row',
+    backgroundColor: '#F0F0F0',
+    borderColor: '#000000',
+    borderWidth: 1,
+    borderRadius: 8,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonEquals: {
+    flex: 1,
+    marginLeft: 10,
+    flexDirection: 'row',
+    backgroundColor: '#ff3232',
+    borderColor: '#ff0000',
+    borderWidth: 1,
+    borderRadius: 8,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonRight: {
+    flex: 1,
+    marginLeft: 10,
+    flexDirection: 'row',
+    backgroundColor: '#c6dcff',
+    borderColor: '#c6dcff',
+    borderWidth: 1,
+    borderRadius: 8,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonsRow: {
+    height: 60,
+    marginTop: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   }
 });
--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
-
+'use strict';
 import {
   AppRegistry
 } from 'react-native';
